d1f0912087de8323e0890705a0780d9b92a2b832
nokchax <nokchax@gmail.com>
2018-12-30 17:05:03 +0900

    feat : yaml to ymlˆ

--- src/main/java/com/nokchax/observer/controller/IncomingController.java
+++ src/main/java/com/nokchax/observer/controller/IncomingController.java
@@ -2,12 +2,12 @@ package com.nokchax.observer.controller;
 
 import com.nokchax.observer.service.GitService;
 import lombok.Setter;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Setter
-@Controller
+@RestController
 public class IncomingController {
     private GitService gitService;
 
