a67d87fb1b1ffc937485a19df6d9892a76e169e3
nokchax <nokchax@gmail.com>
2018-12-31 21:23:32 +0900

    feat : load token value from file resource using @value

--- src/main/java/com/nokchax/observer/service/GitServiceImpl.java
+++ src/main/java/com/nokchax/observer/service/GitServiceImpl.java
@@ -1,11 +1,21 @@
 package com.nokchax.observer.service;
 
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
 
 @Setter
 @Service
 public class GitServiceImpl implements GitService {
+    @Value("${git.token}")
+    private String token;
     private RestTemplate restTemplate;
+
+    @PostConstruct
+    public void test() {
+        System.out.println(token);
+    }
 }
