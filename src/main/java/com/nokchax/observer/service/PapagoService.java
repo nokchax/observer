3adc935cae179af42262420bc7d044a945b862fe
nokchax <nokchax@gmail.com>
2019-01-12 16:28:36 +0900

    feat : make translate service

---
+++ src/main/java/com/nokchax/observer/service/PapagoService.java
@@ -0,0 +1,8 @@
+package com.nokchax.observer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PapagoService implements TranslateService {
+    //todo translate word, sentence using papago api
+}
