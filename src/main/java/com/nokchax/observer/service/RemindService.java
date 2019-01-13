1bb7801870bbe35b09e7dd0d1ce9a83050b75544
nokchax <nokchax@gmail.com>
2019-01-13 20:19:22 +0900

    feat : create remind service interface this service will send til page

---
+++ src/main/java/com/nokchax/observer/service/RemindService.java
@@ -0,0 +1,8 @@
+package com.nokchax.observer.service;
+
+/*
+ send link message of daily TIL page
+ one day ago, three days ago, a week ago, a month ago til page
+ */
+public interface RemindService {
+}
