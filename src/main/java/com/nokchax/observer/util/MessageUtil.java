94442ef8863625ae2a653c447ccfe13ef9bb763a
nokchax <nokchax@gmail.com>
2019-01-17 20:26:57 +0900

    refactor : remove message

--- src/main/java/com/nokchax/observer/util/MessageUtil.java
+++ src/main/java/com/nokchax/observer/util/MessageUtil.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ThreadLocalRandom;
 public class MessageUtil {
     private static final List<String> PRESS_MESSAGES = Arrays.asList(
             "절대 커밋해!",
-            "커밋... 안하시나요?",
             "누구인가? 누가 감히 커밋도 안하고 놀아?",
             "여봐라 이자를 매우 쳐라"
     );
