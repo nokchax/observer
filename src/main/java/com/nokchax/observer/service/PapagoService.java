30d78f78f0214e0a433445327df6a830aff9424d
nokchax <nokchax@gmail.com>
2019-01-18 21:13:37 +0900

    feat : add naver oauth client id and secret in property

--- src/main/java/com/nokchax/observer/service/PapagoService.java
+++ src/main/java/com/nokchax/observer/service/PapagoService.java
@@ -1,12 +1,42 @@
 package com.nokchax.observer.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PapagoService implements TranslateService {
+    @Value("naver.oauth.clientId")
+    private String clientId;
+    @Value("naver.oauth.clientSecret")
+    private String clientSecret;
+
+    private RestTemplate restTemplate;
+
+    public PapagoService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     public String translate(String word) {
-        return "apple";
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded; charset=UTF-8");
+        header.add("X-Naver-Client-Id", clientId);
+        header.add("X-Naver-Client-Secret", clientSecret);
+
+        ResponseEntity<JsonNode> response = restTemplate.exchange(
+                "https://openapi.naver.com/v1/papago/n2mt",
+                HttpMethod.POST,
+                new HttpEntity("source=ko&target=en&text=" + word, header),
+                JsonNode.class
+        );
+        System.out.println(response.getBody().get("message").get("result").get("translatedText").asText());
+
+        return response.getBody().get("message").get("result").get("translatedText").asText();
     }
-    //todo translate word, sentence using papago api
 }
