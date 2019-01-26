cf586546b9f501ac1aed6425a99205ccdfe76699
nokchax <nokchax@gmail.com>
2019-01-26 22:41:18 +0900

    memo : add mvc test link

--- src/test/java/com/nokchax/observer/controller/IncomingControllerTest.java
+++ src/test/java/com/nokchax/observer/controller/IncomingControllerTest.java
@@ -4,13 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+/*
+mock mvc test link
+https://meetup.toast.com/posts/124
+https://stackoverflow.com/questions/18336277/how-to-check-string-in-response-body-with-mockmvc
+ */
 @WebMvcTest
 @RunWith(SpringRunner.class)
 public class IncomingControllerTest {
