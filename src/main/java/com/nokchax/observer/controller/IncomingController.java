14965e8942c15a984bc36d70f5c0c15c0d43d468
nokchax <nokchax@gmail.com>
2019-01-26 22:30:21 +0900

    feat : mock mvc test is difficult T_T

--- src/main/java/com/nokchax/observer/controller/IncomingController.java
+++ src/main/java/com/nokchax/observer/controller/IncomingController.java
@@ -6,10 +6,12 @@ import com.nokchax.observer.service.WebhookService;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /*
@@ -38,21 +40,17 @@ import java.util.Map;
 @Setter
 @RestController
 public class IncomingController {
-    private GitService gitService;
-    private SlackMessengerService slackMessengerService;
-
-    @Autowired
-    public IncomingController(GitService gitService, SlackMessengerService slackMessengerService) {
-        this.gitService = gitService;
-        this.slackMessengerService = slackMessengerService;
-    }
 
     // todo 예외가 발생하면 controller advice에서 잡아서 error 메세지를 체널로 응답한다.
     // todo spring @Controller / @RequestMapping 처럼 필터에서 미리 데이터를 받아서 DISPATCH 시킬 수 있게 만들기
     @PostMapping("/bot")
-    public Map<String, Object> incoming(@RequestBody Map<String, Object> body) {
+    public Map<String, Object> incoming(@RequestBody Map<String, Object> body, HttpServletRequest request) {
         //todo check token from slack filter level
         log.info("{}", body);
+
+        System.out.println(request.toString());
+        System.out.println(request.getHeaderNames());
+        System.out.println(request.getParameterNames());
 
         return body;
     }
--- src/test/java/com/nokchax/observer/controller/IncomingControllerTest.java
+++ src/test/java/com/nokchax/observer/controller/IncomingControllerTest.java
@@ -1,16 +1,18 @@
 package com.nokchax.observer.controller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest
-@SpringBootTest
+@RunWith(SpringRunner.class)
 public class IncomingControllerTest {
     @Autowired
     MockMvc mvc;
@@ -20,6 +22,7 @@ public class IncomingControllerTest {
         mvc.perform(
                 post("/bot")
                 .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"hi\":\"hi\"}")
         );
 
         //.Expect()~~
