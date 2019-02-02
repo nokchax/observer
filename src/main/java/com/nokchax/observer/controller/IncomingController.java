04462d55eb3b35c7468ed97837e480d8e94cbf6a
nokchax <nokchax@gmail.com>
2019-02-02 21:02:57 +0900

    feat : get body using event type class

--- src/main/java/com/nokchax/observer/controller/IncomingController.java
+++ src/main/java/com/nokchax/observer/controller/IncomingController.java
@@ -1,18 +1,13 @@
 package com.nokchax.observer.controller;
 
-import com.nokchax.observer.service.GitService;
-import com.nokchax.observer.service.SlackMessengerService;
-import com.nokchax.observer.service.WebhookService;
+import com.nokchax.observer.domain.EventType;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 
 @Slf4j
@@ -23,14 +18,14 @@ public class IncomingController {
     // todo 예외가 발생하면 controller advice에서 잡아서 error 메세지를 체널로 응답한다.
     // todo spring @Controller / @RequestMapping 처럼 필터에서 미리 데이터를 받아서 DISPATCH 시킬 수 있게 만들기
     @PostMapping("/bot")
-    public Map<String, Object> incoming(@RequestBody Map<String, Object> body, HttpServletRequest request) {
+    public String incoming(@RequestBody EventType eventType, HttpServletRequest request) {
         //todo check token from slack filter level
-        log.info("{}", body);
+        System.out.println(eventType);
 
         System.out.println(request.toString());
         System.out.println(request.getHeaderNames());
         System.out.println(request.getParameterNames());
 
-        return body;
+        return eventType.toString();
     }
 }
