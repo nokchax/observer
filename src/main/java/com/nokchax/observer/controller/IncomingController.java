4bcafcb7a75f4cd6f003742d1ec5de1195f057ff
nokchax <nokchax@gmail.com>
2019-02-03 19:36:29 +0900

    remove sysout

--- src/main/java/com/nokchax/observer/controller/IncomingController.java
+++ src/main/java/com/nokchax/observer/controller/IncomingController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 
 @Slf4j
 @Setter
@@ -18,13 +16,9 @@ public class IncomingController {
     // todo 예외가 발생하면 controller advice에서 잡아서 error 메세지를 체널로 응답한다.
     // todo spring @Controller / @RequestMapping 처럼 필터에서 미리 데이터를 받아서 DISPATCH 시킬 수 있게 만들기
     @PostMapping("/bot")
-    public String incoming(@RequestBody EventType eventType, HttpServletRequest request) {
+    public String incoming(@RequestBody EventType eventType) {
         //todo check token from slack filter level
         System.out.println(eventType);
-
-        System.out.println(request.toString());
-        System.out.println(request.getHeaderNames());
-        System.out.println(request.getParameterNames());
 
         return eventType.toString();
     }
