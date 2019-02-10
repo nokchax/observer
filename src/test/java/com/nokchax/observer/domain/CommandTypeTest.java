235b2b3afd7fe6bc8413db3d55027abb48578d9e
nokchax <nokchax@gmail.com>
2019-02-10 21:54:17 +0900

    feat : add command type test

--- src/test/java/com/nokchax/observer/domain/CommandTypeTest.java
+++ src/test/java/com/nokchax/observer/domain/CommandTypeTest.java
@@ -8,9 +8,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringJUnitConfig
 public class CommandTypeTest {
     @Test
-    public void getCommandWithEventType() {
+    public void getCommandTypeTest_TRANSLATE() {
         CommandType type = CommandType.getCommandType("/번역");
 
         assertThat(type).isEqualTo(CommandType.TRANSLATE);
+    }
+
+    @Test
+    public void getCommandTypeTest_CHECKCOMMIT() {
+        assertThat(CommandType.getCommandType("/커밋체크"))
+                .isEqualTo(CommandType.COMMIT_CHECK);
+    }
+
+    @Test
+    public void getCommandTypeTest_usingNull() {
+        assertThat(CommandType.getCommandType(null))
+                .isEqualTo(CommandType.NONE);
+    }
+
+    @Test
+    public void getCommandTypeTest_usingEmptyString() {
+        assertThat(CommandType.getCommandType(""))
+                .isEqualTo(CommandType.NONE);
     }
 }
