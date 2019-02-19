9f5a97ec840a1050f0b56eb57379a4e747666866
nokchax <nokchax@gmail.com>
2019-02-19 20:56:23 +0900

    feat : add mock bean

--- src/test/java/com/nokchax/observer/controller/SlackMessageControllerTest.java
+++ src/test/java/com/nokchax/observer/controller/SlackMessageControllerTest.java
@@ -1,9 +1,12 @@
 package com.nokchax.observer.controller;
 
+import com.nokchax.observer.component.GitAlarm;
+import com.nokchax.observer.service.TranslateService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +25,10 @@ https://stackoverflow.com/questions/18336277/how-to-check-string-in-response-bod
 public class SlackMessageControllerTest {
     @Autowired
     MockMvc mvc;
+    @MockBean
+    GitAlarm gitAlarm;
+    @MockBean
+    TranslateService papagoService;
 
     //todo mockmvc 로 controller를 테스트 할때 di 시켜줘야할 bean을 등록하는 법 공부
 
@@ -32,6 +39,7 @@ public class SlackMessageControllerTest {
     public void botControllerTest() throws Exception {
         //language=JSON
         String requestJsonBody = "{\"type\" : \"message\", \"text\" : \"hi\", \"user\" : \"WEF123123\", \"ts\" : 1548416095.000200, \"channel\" : \"CEW8123UEW\", \"event_ts\" : 1548416095.000200, \"channel_type\" : \"channel\"}\n";
+        //todo request json body change
         mvc.perform(
                 post("/bot")
                 .contentType(MediaType.APPLICATION_JSON)
