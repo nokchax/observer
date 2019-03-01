b8ba82c089b3ca3be20fe8920651a3d02384d816
nokchax <nokchax@gmail.com>
2019-03-01 20:58:44 +0900

    feat: add getter

--- src/main/java/com/nokchax/observer/component/Message.java
+++ src/main/java/com/nokchax/observer/component/Message.java
@@ -11,18 +11,26 @@ import java.util.List;
 @ConfigurationProperties("message")
 public class Message {
     private List<String> press = new ArrayList<>();
-    //private List<String> celebration = new ArrayList<>();
+    private List<String> celebration = new ArrayList<>();
 
     public void printPress() {
         press.stream().forEach(System.out::println);
+        celebration.stream().forEach(System.out::println);
     }
 
     /*
+    xxxx
     compileOnly('org.springframework.boot:spring-boot-configuration-processor')
     의존성을 추가하지 않으면 getXXX() 가 필요하다.
     하지만 의존성을 추가했을 때는 없어도 동작한다
+    아니네?
+    재 테스트...
      */
-/*    public List<String> getPress() {
-        return this.press;
-    }*/
+    public List<String> getPress() {
+        return press;
+    }
+
+    public List<String> getCelebration() {
+        return celebration;
+    }
 }
