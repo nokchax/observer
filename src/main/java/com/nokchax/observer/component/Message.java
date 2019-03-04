8520587265ffb03a294a6ed87caa95451cb4a7c8
nokchax <nokchax@gmail.com>
2019-03-04 21:43:55 +0900

    feat: add setter and remove new statement

--- src/main/java/com/nokchax/observer/component/Message.java
+++ src/main/java/com/nokchax/observer/component/Message.java
@@ -18,20 +18,9 @@ public class Message {
         press.stream().forEach(System.out::println);
         celebration.stream().forEach(System.out::println);
     }
-
     /*
     xxxx
     compileOnly('org.springframework.boot:spring-boot-configuration-processor')
-    의존성을 추가하지 않으면 getXXX() 가 필요하다.
-    하지만 의존성을 추가했을 때는 없어도 동작한다
-    아니네?
-    재 테스트...
+    getter, setter 추가시 동작
      */
-/*    public List<String> getPress() {
-        return press;
-    }
-
-    public List<String> getCelebration() {
-        return celebration;
-    }*/
 }
