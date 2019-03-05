18c16a4a68cd7ed86ce964dc008cfb0c27d0a704
nokchax <nokchax@gmail.com>
2019-03-05 22:23:28 +0900

    feat: impl message utils methods

--- src/main/java/com/nokchax/observer/component/Message.java
+++ src/main/java/com/nokchax/observer/component/Message.java
@@ -1,11 +1,13 @@
 package com.nokchax.observer.component;
 
 
+import com.nokchax.observer.domain.GitSearchApiResponse;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Data
 @Component
@@ -17,6 +19,18 @@ public class Message {
     public void printPress() {
         press.stream().forEach(System.out::println);
         celebration.stream().forEach(System.out::println);
+    }
+
+    public String getRandomPressMessage() {
+        return press.get(ThreadLocalRandom.current().nextInt(press.size()));
+    }
+
+    public String getRandomCelebrationMessage() {
+        return celebration.get(ThreadLocalRandom.current().nextInt(celebration.size()));
+    }
+
+    public String getMessageByGitApiResponse(GitSearchApiResponse apiResponse) {
+        return apiResponse.hasCommitted() ? getRandomCelebrationMessage() : getRandomPressMessage();
     }
     /*
     xxxx
