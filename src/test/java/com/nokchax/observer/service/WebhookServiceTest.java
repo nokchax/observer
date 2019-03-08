f57b8b736014beee5d624adce0e37a02ec57a51c
nokchax <nokchax@gmail.com>
2019-03-08 22:54:26 +0900

    refactor : optimize import statement

--- src/test/java/com/nokchax/observer/service/WebhookServiceTest.java
+++ src/test/java/com/nokchax/observer/service/WebhookServiceTest.java
@@ -1,6 +1,5 @@
 package com.nokchax.observer.service;
 
-import com.nokchax.observer.util.MessageUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
